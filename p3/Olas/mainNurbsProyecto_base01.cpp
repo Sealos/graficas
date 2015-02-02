1a6981e56e275879f77dc73844f7eb1450c66565
leddkire <leddkire@gmail.com>
2015-02-02 16:09:43 -0430

    Noise y turbulencia agregados

--- p3/Olas/mainNurbsProyecto_base01.cpp
+++ p3/Olas/mainNurbsProyecto_base01.cpp
@@ -3,7 +3,9 @@
 #include <GL\glew.h>
 #include <GL\freeglut.h>
 #include <iostream>
-#include <cmath>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
@@ -12,7 +14,7 @@ using namespace std;
 #define DEF_floorGridZSteps	10.0
 
 float pi = 3.14159f;
-float amplitud = 1.f;
+float amplitud = 2.f;
 float longitud = 3.f;
 float velocidad = -0.1f;
 
@@ -20,6 +22,11 @@ float w = (2*pi)/longitud;
 float constV = velocidad *((2*pi)/longitud);
 
 float decaimiento = amplitud;
+
+float amplitud_ruido = 3.f;
+float offset_ruido = 0.8f;
+float alturaRuido = 1.f;
+float factorTurb = 10.f;
 
 
 GLUnurbsObj *theNurb;
@@ -31,6 +38,130 @@ GLfloat variableKnots[25] = {
 17.0f,18.0f,18.0f,18.0f,18.0f
 };
 GLfloat ctlpoints [21][21][3];
+
+/* coherent noise function over 1, 2 or 3 dimensions */
+/* (copyright Ken Perlin) */
+
+
+
+#define B 0x100
+#define BM 0xff
+
+#define N 0x1000
+#define NP 12   /* 2^N */
+#define NM 0xfff
+
+static int p[B + B + 2];
+static float g3[B + B + 2][3];
+static float g2[B + B + 2][2];
+static float g1[B + B + 2];
+static int start = 1;
+
+static void init(void);
+
+#define s_curve(t) ( t * t * (3. - 2. * t) )
+
+#define lerp(t, a, b) ( a + t * (b - a) )
+
+#define setup(i,b0,b1,r0,r1)\
+	t = vec[i] + N;\
+	b0 = ((int)t) & BM;\
+	b1 = (b0+1) & BM;\
+	r0 = t - (int)t;\
+	r1 = r0 - 1.;
+
+float noise2(float vec[2])
+{
+	int bx0, bx1, by0, by1, b00, b10, b01, b11;
+	float rx0, rx1, ry0, ry1, *q, sx, sy, a, b, t, u, v;
+	register int i, j;
+
+	if (start) {
+		start = 0;
+		init();
+	}
+
+	setup(0, bx0,bx1, rx0,rx1);
+	setup(1, by0,by1, ry0,ry1);
+
+	i = p[ bx0 ];
+	j = p[ bx1 ];
+
+	b00 = p[ i + by0 ];
+	b10 = p[ j + by0 ];
+	b01 = p[ i + by1 ];
+	b11 = p[ j + by1 ];
+
+	sx = s_curve(rx0);
+	sy = s_curve(ry0);
+
+#define at2(rx,ry) ( rx * q[0] + ry * q[1] )
+
+	q = g2[ b00 ] ; u = at2(rx0,ry0);
+	q = g2[ b10 ] ; v = at2(rx1,ry0);
+	a = lerp(sx, u, v);
+
+	q = g2[ b01 ] ; u = at2(rx0,ry1);
+	q = g2[ b11 ] ; v = at2(rx1,ry1);
+	b = lerp(sx, u, v);
+
+	return lerp(sy, a, b);
+}
+
+static void normalize2(float v[2])
+{
+	float s;
+
+	s = sqrt(v[0] * v[0] + v[1] * v[1]);
+	v[0] = v[0] / s;
+	v[1] = v[1] / s;
+}
+static void init_perlin(void)
+{
+	int i, j, k;
+
+	for (i = 0 ; i < B ; i++) {
+		p[i] = i;
+
+		g1[i] = (float)((rand() % (B + B)) - B) / B;
+
+		for (j = 0 ; j < 2 ; j++)
+			g2[i][j] = (float)((rand() % (B + B)) - B) / B;
+		normalize2(g2[i]);
+	}
+
+	while (--i) {
+		k = p[i];
+		p[i] = p[j = rand() % B];
+		p[j] = k;
+	}
+
+	for (i = 0 ; i < B + 2 ; i++) {
+		p[B + i] = p[i];
+		g1[B + i] = g1[i];
+		for (j = 0 ; j < 2 ; j++)
+			g2[B + i][j] = g2[i][j];
+		for (j = 0 ; j < 3 ; j++)
+			g3[B + i][j] = g3[i][j];
+	}
+}
+
+
+double turbulence(double x, double y, double size)
+{
+    double value = 0.0, initialSize = size;
+    float noise[2];
+	
+    while(size >= 1)
+    {
+		noise[0] = x/size;
+		noise[1] = y/size;
+        value += noise2(&noise[0]) * size;
+        size /= 2.0;
+    }
+    
+    return(128.0 * value / initialSize);
+}
 
 void ejesCoordenada() {
 	
@@ -116,7 +247,7 @@ void init(){
    gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 15.0);
    gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
 
-	
+	init_perlin();
 
 }
 
@@ -160,7 +291,7 @@ void render(){
     GLfloat light_ambient[] = { 0.0, 0.0, 0.2, 1.0 };
 	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
 	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light_position[] = { 3, -5, 3, 1.0 };
+	GLfloat light_position[] = { -10, -5, 0.0, 1.0 };
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
@@ -260,8 +391,11 @@ void calcularOla(){
 			if(ampl < 0){
 				ampl = 0;
 			}
-
-			ctlpoints[i][j][1] = (ampl * sin((longitudV * w) + (tiempo*constV)));
+			float noise[2];
+			noise [0] = ctlpoints[i][j][0] * amplitud_ruido + offset_ruido;
+			noise [1] = ctlpoints[i][j][2] * amplitud_ruido + offset_ruido;
+			float ruido = alturaRuido*0.005* turbulence(noise[0],noise[1],factorTurb);
+			ctlpoints[i][j][1] = (ampl * sin((longitudV * w) + (tiempo*constV))) + ruido;
 		}
 	}
 }
