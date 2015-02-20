8ad313611274bec1d5c54a12e17e9c0b13c64ad1
Stefano De Colli <sdecolli@gmail.com>
2015-02-19 21:28:32 -0430

    .gitignore

--- p5Texturas/p5Texturas/mainProyectoOpenglII.cpp
+++ p5Texturas/p5Texturas/mainProyectoOpenglII.cpp
@@ -67,12 +67,48 @@ void changeViewport(int w, int h) {
 
 void init(){
 
-
-	
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
-   
+	glEnable(GL_TEXTURE_2D);
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+
+	glGenTextures(1, &texName[0]);
+	glBindTexture(GL_TEXTURE_2D, texName[0]);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	images[0] = glmReadPPM("texAO_plano.ppm", &iwidth[0], &iheight[0]);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth[0], iheight[0], 0, GL_RGB, GL_UNSIGNED_BYTE, images[0]);
+
+	glGenTextures(1, &texName[2]);
+	glBindTexture(GL_TEXTURE_2D, texName[2]);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	images[2] = glmReadPPM("texAO_bunny.ppm", &iwidth[2], &iheight[2]);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth[2], iheight[2], 0, GL_RGB, GL_UNSIGNED_BYTE, images[2]);
+
+
+	glGenTextures(1, &texName[1]);
+	glBindTexture(GL_TEXTURE_2D, texName[1]);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	images[1] = glmReadPPM("texAO_columna.ppm", &iwidth[1], &iheight[1]);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iwidth[1], iheight[1], 0, GL_RGB, GL_UNSIGNED_BYTE, images[1]);
 
 }
 
